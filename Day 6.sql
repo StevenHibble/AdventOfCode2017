d7b6a6ac637bdcc3b82f32e4aadfe1e5256444bd
StevenHibble <stevenhibble@gmail.com>
2017-12-20 21:59:24 -0700

    All good
    I got the first answer easily, but then had to figure out how to capture how long the loop was. I added an ID field for that.

--- Day 6.sql
+++ Day 6.sql
@@ -4,19 +4,32 @@ DROP TABLE IF EXISTS #Banks;
 SELECT BankID = ID
      , BlockCount = CONVERT(INT, String)
 INTO #Banks
-FROM Manage.Tools.SplitString(@Input, CHAR(9));
+FROM Utility.Tools.SplitString(@Input, CHAR(9));
 
-DECLARE @Configurations TABLE ([Configuration] VARCHAR(8000) PRIMARY KEY);
+DECLARE @Configurations TABLE (ID INT IDENTITY(1,1)
+                             , [Configuration] VARCHAR(8000) PRIMARY KEY);
 
 DECLARE @BiggestBank INT
 DECLARE @BlockCount INT
-DECLARE @Counter INT = 1;
+DECLARE @Counter INT = 0;
 
-WHILE @Counter <= 4
+SET NOCOUNT ON;
+WHILE 1 = 1
   BEGIN
+    
+    -- Try to insert a new configuration
+    -- If it's already in @Configurations, then don't do anything
     INSERT INTO @Configurations
-    SELECT STRING_AGG(@BlockCount, ',') WITHIN GROUP (ORDER BY BankID)
-    FROM #Banks;
+    SELECT [Configuration]
+    FROM (SELECT [Configuration] = STRING_AGG(BlockCount, ',') WITHIN GROUP (ORDER BY BankID)
+          FROM #Banks) b
+    WHERE NOT EXISTS (SELECT 1
+                      FROM @Configurations c
+                      WHERE c.[Configuration] = b.[Configuration]);
+    
+    -- If the configuration didn't insert (it's already been seen), then break
+    IF @@ROWCOUNT = 0
+      BREAK;
 
     SELECT TOP 1
            @BiggestBank = BankID
@@ -25,9 +38,18 @@ WHILE @Counter <= 4
     ORDER BY BlockCount DESC
            , BankID;
     
-    UPDATE b
-    SET BlockCount = IIF(BankID = @BiggestBank, 0, BlockCount) + (@BlockCount/16) + IIF(StepsAway <= @BlockCount%16, 1, 0)
-    FROM [#Banks] b
-         CROSS APPLY (SELECT StepsAway = IIF(BankID > @BiggestBank, BankID - @BiggestBank, BankID + 16 - @BiggestBank)) sa;
-    SET @Counter += 1
+    UPDATE b         
+    SET BlockCount = IIF(BankID = @BiggestBank, 0, BlockCount) -- If it's the original block, then reset to 0, else start at the current BlockCount
+                   + (@BlockCount / 16) -- Then add to each block the total number of times the original BlockCount would go around 16 (e.g. 1-15 -> 0; 16-31 -> 1; 32-47 -> 2; etc.)
+                   + IIF(StepsAway <= @BlockCount % 16, 1, 0) -- Then add the remaining blocks to the first X blocks after the original block
+    FROM [#Banks] b                          
+         CROSS APPLY (SELECT StepsAway = IIF(BankID > @BiggestBank, BankID - @BiggestBank, BankID + 16 - @BiggestBank)) sa; -- If the block is BEFORE the original block, add 16 to treat as a wrap around
+
+    SET @Counter += 1;
   END;
+
+SELECT Solution1 = @Counter;
+
+SELECT Solution2 = @Counter + 1 - ID
+FROM @Configurations
+WHERE [Configuration] = (SELECT STRING_AGG(BlockCount, ',') WITHIN GROUP (ORDER BY BankID) FROM #Banks);
